022882e46a643e8c193e242b49ea40310c4fddbd
Jerrik Shaw <jlshaw117@gmail.com>
2018-11-15 10:52:17 -0800

    Add UserController with methods for new and create

--- music_app/app/controllers/users_controller.rb
+++ music_app/app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login(@user)
+      # redirect_to 
+    else
+      Flash.now[:errors] = "Invalid credentials"
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
