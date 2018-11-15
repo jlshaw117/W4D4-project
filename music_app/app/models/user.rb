2cbe982d30856838e51a409e2ecdb86202252933
Jerrik Shaw <jlshaw117@gmail.com>
2018-11-15 10:36:39 -0800

    Add auth pattern to user.rb

--- music_app/app/models/user.rb
+++ music_app/app/models/user.rb
@@ -11,4 +11,37 @@
 #
 
 class User < ApplicationRecord
+
+  validations :email, :session_token, presence: true, uniqueness: true
+  validations :password_digest, presence: true,
+  validations :password, length: {minimum: 6, allow_nil: true}
+  before_validation :ensure_session_token
+
+  attr_reader :password
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    user.is_password(password) ? user : nil
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
 end
